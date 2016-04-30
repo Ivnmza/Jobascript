4bb74f1b3cf5c5444cfb29fffc6fc08c9ab44317
Ivan Meza <ivn.mza@gmail.com>
2016-04-30 16:22:57 -0700

    identified issue in data binding

--- client/src/communications/controller.js
+++ client/src/communications/controller.js
@@ -1,25 +1,41 @@
-module.exports = function ($scope, Comm, currentCompany) {
-  console.log(currentCompany);
+module.exports = function ($scope, Comm, currentCompany, emails, $state) {
+  // console.log(currentCompany);
+  // var theEmails = [];
+  $scope.myEmails = emails;
 
-  var googleScript = document.createElement('script');
-  googleScript.setAttribute('src', 'https://apis.google.com/js/client.js');
-  googleScript.setAttribute('id', 'onetime');
-  if (!document.getElementById('onetime')) {
-    document.head.appendChild(googleScript);
-  }
 
-  $scope.emails = [];
   // need to check auth first
   
 
-  $scope.auth = function() {
-    console.log('check auth');
-    Comm.checkAuth().then(function () {
-      console.log('get emails');
-      Comm.getEmails(currentCompany).then(function (emails) {
-        console.log('emails here: ', emails);
-        $scope.emails = emails;
-      });
+  // $scope.auth = function() {
+  //   console.log('check auth');
+  //   Comm.checkAuth().then(function () {
+  //     console.log('get emails');
+  //     Comm.getEmails(currentCompany).then(function (emails) {
+  //       console.log('emails here: ', emails);
+  //       $scope.emails = emails;
+  //       console.log('$scope.emails ', $scope.emails);
+  //     });
+  //   });
+  // };
+
+  $scope.auth = function () {
+    Comm.checkAuth()
+    .then(function() {
+      console.log('authoized');
+      $state.reload();
     });
   };
+
+  $scope.getEmails = function () {
+    Comm.getEmails(currentCompany).then(function (emails) {
+      console.log('b ', $scope.myEmails);
+      updateEmails(emails);
+      console.log('a ', $scope.myEmails);
+    });
+  };
+
+  function updateEmails(emails) {
+    $scope.myEmails = emails;
+  }
 };
